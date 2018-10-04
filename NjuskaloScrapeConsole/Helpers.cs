9a14df819b6ed43a7f6a5454bc49c9c32a10fb82
Mario Mestrovic <mario.mestrovic@axilis.com>
2018-10-04 22:18:13 +0200

    Add project files.

---
+++ NjuskaloScrapeConsole/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NjuskaloScrapeConsole
+{
+    public static class Helpers
+    {
+        public static bool HasValue(this object source)
+        {
+            return source == null ? false : (source is string ? !string.IsNullOrWhiteSpace(source as string) : true);
+        }
+    }
+}
